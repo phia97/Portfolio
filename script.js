4bc7e4c4890f0efc3b243e2b69f0f6036e9d1b8e
David _ <phiaphia97@yahoo.com>
2025-12-09 18:05:57 -0500

    finalize script.js

---
+++ script.js
@@ -0,0 +1,139 @@
+function mouseClickArt1() {
+    document.getElementById("art1").style.width = "40%";
+    document.getElementById("art1").style.marginLeft = "30%";
+}
+
+function unhoverOverArt1() {
+    document.getElementById("art1").style.width = "30%";
+    document.getElementById("art1").style.marginLeft = "35%";
+}
+
+function mouseClickArt2() {
+    document.getElementById("art2").style.width = "50%";
+    document.getElementById("art2").style.marginLeft = "25%";
+}
+
+function unhoverOverArt2() {
+    document.getElementById("art2").style.width = "30%";
+    document.getElementById("art2").style.marginLeft = "35%";
+}
+
+function mouseClickArt3() {
+    document.getElementById("art3").style.width = "40%";
+    document.getElementById("art3").style.marginLeft = "30%";
+}
+
+function unhoverOverArt3() {
+    document.getElementById("art3").style.width = "30%";
+    document.getElementById("art3").style.marginLeft = "35%";
+}
+
+function mouseClickArt4() {
+    document.getElementById("art4").style.width = "40%";
+    document.getElementById("art4").style.marginLeft = "30%";
+}
+
+function unhoverOverArt4() {
+    document.getElementById("art4").style.width = "30%";
+    document.getElementById("art4").style.marginLeft = "35%";
+}
+
+function mouseClickArt5() {
+    document.getElementById("art5").style.width = "40%";
+    document.getElementById("art5").style.marginLeft = "30%";
+}
+
+function unhoverOverArt5() {
+    document.getElementById("art5").style.width = "30%";
+    document.getElementById("art5").style.marginLeft = "35%";
+}
+
+function mouseClickArt6() {
+    document.getElementById("art6").style.width = "40%";
+    document.getElementById("art6").style.marginLeft = "30%";
+}
+
+function unhoverOverArt6() {
+    document.getElementById("art6").style.width = "30%";
+    document.getElementById("art6").style.marginLeft = "35%";
+}
+
+function mouseClickArt7() {
+    document.getElementById("art7").style.width = "50%";
+    document.getElementById("art7").style.marginLeft = "25%";
+}
+
+function unhoverOverArt7() {
+    document.getElementById("art7").style.width = "30%";
+    document.getElementById("art7").style.marginLeft = "35%";
+}
+
+function mouseClickArt8() {
+    document.getElementById("art8").style.width = "40%";
+    document.getElementById("art8").style.marginLeft = "30%";
+}
+
+function unhoverOverArt8() {
+    document.getElementById("art8").style.width = "30%";
+    document.getElementById("art8").style.marginLeft = "35%";
+}
+
+function mouseClickArt9() {
+    document.getElementById("art9").style.width = "40%";
+    document.getElementById("art9").style.marginLeft = "30%";
+}
+
+function unhoverOverArt9() {
+    document.getElementById("art9").style.width = "30%";
+    document.getElementById("art9").style.marginLeft = "35%";
+}
+
+function mouseClickArt10() {
+    document.getElementById("art10").style.width = "40%";
+    document.getElementById("art10").style.marginLeft = "30%";
+}
+
+function unhoverOverArt10() {
+    document.getElementById("art10").style.width = "30%";
+    document.getElementById("art10").style.marginLeft = "35%";
+}
+
+function mouseClickArt11() {
+    document.getElementById("art11").style.width = "40%";
+    document.getElementById("art11").style.marginLeft = "30%";
+}
+
+function unhoverOverArt11() {
+    document.getElementById("art11").style.width = "30%";
+    document.getElementById("art11").style.marginLeft = "35%";
+}
+
+function mouseClickArt12() {
+    document.getElementById("art12").style.width = "50%";
+    document.getElementById("art12").style.marginLeft = "25%";
+}
+
+function unhoverOverArt12() {
+    document.getElementById("art12").style.width = "30%";
+    document.getElementById("art12").style.marginLeft = "35%";
+}
+
+function mouseClickArt13() {
+    document.getElementById("art13").style.width = "40%";
+    document.getElementById("art13").style.marginLeft = "30%";
+}
+
+function unhoverOverArt13() {
+    document.getElementById("art13").style.width = "30%";
+    document.getElementById("art13").style.marginLeft = "35%";
+}
+
+function mouseClickArt14() {
+    document.getElementById("art14").style.width = "50%";
+    document.getElementById("art14").style.marginLeft = "25%";
+}
+
+function unhoverOverArt14() {
+    document.getElementById("art14").style.width = "30%";
+    document.getElementById("art14").style.marginLeft = "35%";
+}
